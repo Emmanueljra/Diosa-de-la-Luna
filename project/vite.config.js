0a2423479d2a29a337705c869909b7a032909835
Emmanuel Rodriguez <emmanuel.rodrias@gmail.com>
2025-07-08 20:34:09 -0600

    Add files via upload

---
+++ project/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        cabana: 'cabana.html',
+        contacto: 'contacto.html',
+        terminos: 'terminos.html',
+        'tours-nacionales': 'tours-nacionales.html',
+        'tours-internacionales': 'tours-internacionales.html'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
